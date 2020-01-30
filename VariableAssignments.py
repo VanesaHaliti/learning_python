25f16b4ce4953c405ebbd16567c824352053b9cc
Vanesa Haliti <vh40258@ubt-uni.net>
2020-01-30 11:31:44 +0100

    did some simple exercises related to variable assignments

---
+++ VariableAssignments.py
@@ -0,0 +1,16 @@
+a = 5
+print(a)    
+a = 10
+print(a) #output: 10
+print(a + a)
+a = a + a #reassign it
+print(a)
+print(a + a)
+print(type(a))
+a = 20.1
+print(type(a))
+
+my_income = 100
+tax_rate = 0.1
+my_income = my_income * tax_rate
+print(my_income)
