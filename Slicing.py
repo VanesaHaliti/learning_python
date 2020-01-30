e71a2a3349b8542c6fedb7f2cc16a4f96fb05fa6
Vanesa Haliti <vh40258@ubt-uni.net>
2020-01-30 11:44:10 +0100

    string manipulations with slicing

---
+++ Slicing.py
@@ -0,0 +1,15 @@
+print("------Exercise 1-------")
+
+myText = "String manipulation is further discussed in Chapter 21."
+print(myText[-11:])
+print(myText[7:19])
+print(myText[:-14])
+print(myText[44])
+
+print("------Exercise 2-------")
+
+myTxt = 'Python is an interesting and useful language for numerical analysis!'
+print(myTxt[0:6])
+print(myTxt[-1])
+print(myTxt[-9:-1])
+print(myTxt[::-1])
