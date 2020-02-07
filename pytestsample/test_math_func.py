5be05c6c5b7a3980780ed3585209e33549adc927
Vanesa Haliti <vh40258@ubt-uni.net>
2020-02-07 10:43:40 +0100

    Wrote two simple add and product functions

---
+++ pytestsample/test_math_func.py
@@ -0,0 +1,16 @@
+import math_func
+
+
+def test_add():
+    assert math_func.add(7, 3) == 10 # assert means that you want to check if this code is returning a certain result
+    assert math_func.add(7) == 9
+    assert math_func.add(5) == 7
+
+
+def test_product():
+    assert math_func.product(5, 5) == 25
+    assert math_func.product(5) == 18
+    assert math_func.product(7) == 14
+
+
+
